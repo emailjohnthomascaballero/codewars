bd403b51f782587e1d7bdb0c5b1fec84188cc0bd
John <emailjohnthomascaballero@gmail.com>
2024-02-19 23:57:34 +0800

    Day 5

--- katas/day-5.js
+++ katas/day-5.js
@@ -5,5 +5,11 @@
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
 function repeatStr (n, s) {
-  return '';
+  // let integer = n;
+  // let string = s;
+  
+  return s.repeat(n);
 }
+
+console.log(repeatStr(3, "*"))
+console.log(repeatStr(2, "ha "))

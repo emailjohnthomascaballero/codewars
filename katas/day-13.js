ce310dade1999387aac293396513ddf427444db5
John <emailjohnthomascaballero@gmail.com>
2024-02-27 16:21:35 +0800

    Day 13

--- katas/day-13.js
+++ katas/day-13.js
@@ -9,6 +9,32 @@
 // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
-function addBinary(a,b) {
-  
-}
+// my first answer but it's not working
+// function addBinary(a, b) {
+//   // convert decimal to binary
+//   let binaryOne = (a >>> 0).toString(2);
+//   let binaryTwo = (b >>> 0).toString(2);
+//   // add binary
+//   return (parseInt(binaryOne, 2) + parseInt(binaryTwo, 2)).toString(2);
+// }
+
+// console.log(addBinary(1, 1));
+// console.log(addBinary(5, 9));
+// // check if string
+// console.log(typeof addBinary(5, 9));
+
+// my second answer but its working!
+// function addBinary(a, b) {
+//   let add = a + b;
+//   return add.toString(2);
+// }
+
+// my third answer but in shorter way
+// function addBinary(a, b) {
+//   return (a + b).toString(2);
+// }
+
+// my final answer one line code
+addBinary = (a, b) => (a + b).toString(2);
+
+console.log(addBinary(5, 9))

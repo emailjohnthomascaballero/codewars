8090fac629921d65ca477087f28c455af5884ffc
John <emailjohnthomascaballero@gmail.com>
2024-03-18 15:50:43 +0800

    Day 28

---
+++ katas/fundamentals-return.js
@@ -0,0 +1,50 @@
+// Fundamentals: Return
+
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+function exponent(a, b) {
+  return a ** b;
+}
+
+function subt(a, b) {
+  return a - b;
+}
+
+console.log(add(1, 2));
+console.log(divide(1, 2));
+console.log(multiply(1, 2));
+console.log(mod(1, 2));
+console.log(exponent(1, 2));
+console.log(subt(1, 2));

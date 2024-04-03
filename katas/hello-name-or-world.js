7ac8ac0c29bca7e81e317532e77452979cbe2e51
John <emailjohnthomascaballero@gmail.com>
2024-04-03 17:31:39 +0800

    Day 43

---
+++ katas/hello-name-or-world.js
@@ -0,0 +1,25 @@
+// Hello, Name or World!
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Examples:
+
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given
+//   or `name` = ""        => return "Hello, World!"
+
+function hello(name) {
+  if (name == "" || name == null) {
+    return "Hello, World!";
+  } else {
+    return "Hello, " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+  }
+}
+
+console.log(hello("john"));
+console.log(hello("aliCE"));
+console.log(hello(""));
+console.log(hello());

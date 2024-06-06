207499abfcbf1372f980f864d9eee09046d28cd9
John <emailjohnthomascaballero@gmail.com>
2024-06-07 01:05:31 +0800

    Day 104

---
+++ katas/kata-example-twist.js
@@ -0,0 +1,9 @@
+function loopCodewars() {
+  var websites = [];
+  for (let index = 0; index < 1_000; index++) {
+    websites.push("codewars");
+  }
+  return websites;
+}
+
+console.log(loopCodewars());

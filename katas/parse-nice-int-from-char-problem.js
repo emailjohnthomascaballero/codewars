d678e53e32d5275ebe30dc9d0a62d4f201724820
John <emailjohnthomascaballero@gmail.com>
2024-06-24 19:49:16 +0800

    Day 116

--- katas/parse-nice-int-from-char-problem.js
+++ katas/parse-nice-int-from-char-problem.js
@@ -7,9 +7,14 @@
 // Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
 function getAge(inputString) {
-  return `${inputString} ${Math.floor(Math.random() * 9)}`;
+  // return `${inputString} ${Math.floor(Math.random() * 9)}`;
   // return `${inputString} ${Math.floor(Math.random() * 10)} years old`;
+  // return `${Math.floor(Math.random() * 10)} years old`;
+  // return typeof Math.floor(Math.random() * 10);
+  // return 4;
+  // return `${inputString} ${Math.floor(Math.random() * 10)} years old`;
+  return parseInt(inputString.charAt(0));
   // return the girl's correct age as an integer. Happy coding :)
 }
 
-console.log(getAge("How old are you?"))
+console.log(getAge("4 years old"))

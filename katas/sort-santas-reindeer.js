2518bba4bcde4ca270a890556bc0a61fabb15070
John <emailjohnthomascaballero@gmail.com>
2024-06-29 23:06:27 +0800

    Day 121

--- katas/sort-santas-reindeer.js
+++ katas/sort-santas-reindeer.js
@@ -48,13 +48,6 @@ function sortReindeer(reindeerNames) {
     }
 })
 };
-    // a.split(' ')[1] > b.split(' ')[1] ? 1 : -1;
-
-  // let lastName = reindeerNames.slice(' ').sort((a, b) => a.split(' ')[1] > b.split(' ')[1] ? 1 : -1);
-  // return lastName.slice(' ').sort((a, b) => a.split(' ')[0] > b.split(' ')[0] ? -1 : 1);
-
-
-// const sortReindeer = (reindeerNames) => reindeerNames.slice(' ').sort((a, b) => a.split(' ')[1] > b.split(' ')[1] ? 1 : -1);
 
 console.log(sortReindeer(["Dasher Tonoyan", "Dancer Moore", "Prancer Chua", "Vixen Hall", "Comet Karavani", "Cupid Foroutan", "Donder Jonker", "Blitzen Claus", 'Kenjiro Mori', 'Susumu Mori', 'Akira Mori' ]));
 

db71b70f49570c882f0982dcf22a5ba2d3a7c705
John <emailjohnthomascaballero@gmail.com>
2024-07-04 22:23:58 +0800

    Day 125

--- katas/is-he-gonna-survive.js
+++ katas/is-he-gonna-survive.js
@@ -5,9 +5,12 @@
 // Return true if yes, false otherwise :)
 
 function hero(bullets, dragons) {
-  //Get Coding!
+  return bullets >= dragons * 2;
 }
 
 console.log(hero(10, 5));
 console.log(hero(7, 4));
 console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1))

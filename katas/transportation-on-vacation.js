989ed1f68b74e4a37021d0d1e96c386b579ac2f6
John <emailjohnthomascaballero@gmail.com>
2024-07-05 23:20:23 +0800

    Day 126

---
+++ katas/transportation-on-vacation.js
@@ -0,0 +1,35 @@
+// Transportation on vacation
+
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+// function rentalCarCost(d) {
+//   let total = 40 * d;
+//   if (d < 3) {
+//     return total;
+//   } else if (d < 7) {
+//     return total - 20;
+//   } else {
+//     return total - 50;
+//   }
+// }
+
+// function rentalCarCost(d) {
+//   return d < 3 ? 40 * d : d < 7 ? 40 * d - 20 : 40 * d - 50;
+// }
+
+const rentalCarCost = (d) => d < 3 ? 40 * d : d < 7 ? 40 * d - 20 : 40 * d - 50;
+
+console.log(rentalCarCost(1));
+console.log(rentalCarCost(2));
+
+console.log(rentalCarCost(3));
+console.log(rentalCarCost(4));
+
+console.log(rentalCarCost(7));
+console.log(rentalCarCost(8));

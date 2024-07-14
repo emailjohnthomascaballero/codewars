8c42a8c90fd1a7dfde6d58ec62a70d61803007c1
John <emailjohnthomascaballero@gmail.com>
2024-07-14 23:17:40 +0800

    Day 133

--- katas/if-you-cant-sleep-just-count-sheep.js
+++ katas/if-you-cant-sleep-just-count-sheep.js
@@ -6,7 +6,14 @@
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
 var countSheep = function (num) {
-  //your code here
+  const sheep = [];
+  for (let index = 0; index < num; index++) {
+    // const element = sheep[index];
+    sheep.push(`${index + 1} sheep...`);
+  }
+  return sheep.join("");
 };
 
+console.log(countSheep(0));
+console.log(countSheep(1));
 console.log(countSheep(3));

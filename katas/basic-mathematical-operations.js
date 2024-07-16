3bb07ba44c8cc0eeb186ea4abc7a79d0abd37dab
John <emailjohnthomascaballero@gmail.com>
2024-07-16 23:48:04 +0800

    Day 135

--- katas/basic-mathematical-operations.js
+++ katas/basic-mathematical-operations.js
@@ -13,7 +13,20 @@
 
 function basicOp(operation, value1, value2){
   //Code
-  return 0;
+  // if (operation === '+') {
+  //   return value1 + value2;
+  // } else if (operation === '-') {
+  //   return value1 - value2;
+  // } else if (operation === '*') {
+  //   return value1 * value2;
+  // } else if (operation === '/') {
+  //   return value1 / value2;
+  // }
+
+  return operation == '+' ? value1 + value2
+    : operation == '-' ? value1 - value2
+      : operation == '*' ? value1 * value2
+        : value1 / value2
 }
 
 console.log(basicOp('+', 4, 7));

aeaf3afc6e6418fd0aff754433370b2d86631854
John <emailjohnthomascaballero@gmail.com>
2024-07-26 22:55:53 +0800

    Day 144

---
+++ katas/find-numbers-which-are-divisible-by-given-number.js
@@ -0,0 +1,16 @@
+// Find numbers which are divisible by given number
+
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+// Example(Input1, Input2 --> Output)
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+function divisibleBy(numbers, divisor) {
+  
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2));
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3));
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4));
+console.log(divisibleBy([0], 4));
+console.log(divisibleBy([1, 3, 5], 2));

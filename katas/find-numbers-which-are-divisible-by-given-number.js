903714587806054a07f7d1c40300b24dbf5bc84b
John <emailjohnthomascaballero@gmail.com>
2024-07-28 08:40:56 +0800

    Day 145

--- katas/find-numbers-which-are-divisible-by-given-number.js
+++ katas/find-numbers-which-are-divisible-by-given-number.js
@@ -6,11 +6,11 @@
 // [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
 
 function divisibleBy(numbers, divisor) {
-  
+  return numbers.map( x => x % divisor == 0 ? x : null).filter(x => x != null);
 }
 
-console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2));
-console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3));
-console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4));
-console.log(divisibleBy([0], 4));
-console.log(divisibleBy([1, 3, 5], 2));
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // [2,4,6]
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3)); // [3,6]
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4)); // [0,4]
+console.log(divisibleBy([0], 4)); // [0]
+console.log(divisibleBy([1, 3, 5], 2)); // []

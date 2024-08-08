b32cc1c37ef534cdb818eb9d78fbe8681b7341b4
John <emailjohnthomascaballero@gmail.com>
2024-08-08 11:04:09 +0800

    Day 156

--- katas/training-js-3-basic-data-types--string.js
+++ katas/training-js-3-basic-data-types--string.js
@@ -1,13 +1,19 @@
-// Training JS #2: Basic data types--Number
+// Training JS #3: Basic data types--String
+
+// In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+
+// String can use operators +, Connect two or more strings together.
 
 // Task
-// I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+// misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
 
 // When you have finished the work, click "Run Tests" to see if your code is working properly.
 
-// In the end, click "Submit" to submit your code pass this kata.
+// misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
 
-// Series:
+// If everything is right, click "Submit" again to submit your code pass this kata.
+
+// Series
 // ( ↑↑↑ Click the link above can get my newest kata list, Please add it to your favorites)
 
 // #1: create your first JS function helloWorld
@@ -53,46 +59,38 @@
 // #41: Regular Expression--""
 // #42: Regular Expression--(?:), (?=) and (?!)
 
-let v1 = 50,
-    v2 = 100,
-    v3 = 150,
-    v4 = 200,
-    v5 = 2,
-    v6 = 250;
-
-function equal1(){
-  let a = v1,   
-      b = v1;   
-  return a + b;
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
 }
 
-//Please refer to the example above to complete the following functions
-function equal2(){
-  let a =  v3, //set number value to a
-      b =  v1; //set number value to b
-  return a - b;
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
 }
 
-function equal3(){
-  let a =  v1, //set number value to a
-      b =  v5; //set number value to b
-  return a * b;
-}
+console.log(Dad());
+console.log(Bee());
+console.log(banana());
 
-function equal4(){
-  let a =  v4, //set number value to a
-      b =  v5; //set number value to b
-  return a / b;
-}
-
-function equal5(){
-  let a =  v6, //set number value to a
-      b =  v3; //set number value to b
-  return a % b;
-}
-
-console.log(equal1());
-console.log(equal2());
-console.log(equal3());
-console.log(equal4());
-console.log(equal5());
+console.log(answer1());
+console.log(answer2());
+console.log(answer3());

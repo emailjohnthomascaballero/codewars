89d4b705d51c3add98744995b6dad7bb32ff4f57
John <emailjohnthomascaballero@gmail.com>
2024-08-19 20:15:43 +0800

    Day 166

--- katas/training-js-10-loop-statement--for.js
+++ katas/training-js-10-loop-statement--for.js
@@ -97,9 +97,20 @@
 
 function pickIt(arr){
   var odd=[],even=[];
-  //coding here
   
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
   
   return [odd,even];
 }
 
+console.log(pickIt([1,2]));
+console.log(pickIt([1,2,3]))
+console.log(pickIt([10,20,30]))
+console.log(pickIt([11,21,31]))
+console.log(pickIt([8,1,5,4,6,1,1]))

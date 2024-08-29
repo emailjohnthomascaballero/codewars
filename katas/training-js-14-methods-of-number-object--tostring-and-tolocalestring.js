bf38ca90195a38130f740ddf64a7c74abb15ec53
John <emailjohnthomascaballero@gmail.com>
2024-08-29 10:58:51 +0800

    Day 176

--- katas/training-js-14-methods-of-number-object--tostring-and-tolocalestring.js
+++ katas/training-js-14-methods-of-number-object--tostring-and-tolocalestring.js
@@ -90,8 +90,36 @@
 // #42: Regular Expression--(?:), (?=) and (?!)
 
 function colorOf(r,g,b){
-  //coding here
-  
+  // return r.toString(16).padStart(2,"0")+g.toString(16).padStart(2,"0")+b.toString(16).padStart(2,"0")
+  return `#${r.toString(16).padStart(2,"0")}${g.toString(16).padStart(2,"0")}${b.toString(16).padStart(2,"0")}`
+
+  // if (g.length < 2) {
+  //   return true
+  // }
+
+  // let rr=r.toString(16);
+  //     gg=g.toString(16);
+  //     bb=b.toString(16);
+  // let arr = []
+  // if (rr.length < 2) {
+  //   let rrr="0"+rr;
+  //   arr.push(rrr)
+  // } 
+  // if (gg.length < 2) {
+  //   let ggg="0"+gg;
+  //   arr.push(ggg)
+  // }
+  // if (bb.length < 2) {
+  //   let bbb="0"+bb;
+  //   arr.push(bbb)
+  // }
+  // return `#${rrr}${ggg}${bbb}`
+  // return txt.length
+  // return arr;
+
+
 }
 
 console.log(colorOf(255,0,0))
+console.log(colorOf(0,111,0))
+console.log(colorOf(1, 2 ,3))

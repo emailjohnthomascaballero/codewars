75f675a8f912aef5faad667d303b5830d867250c
JOHN THOMAS F. CABALLERO <emailjohnthomascaballero@gmail.com>
2024-11-09 10:46:07 +0800

    Delete katas directory
    deleted katas folder

--- katas/you-cant-code-under-pressure-#1.js
+++
@@ -1,9 +0,0 @@
-// You Can't Code Under Pressure #1
-
-// Code as fast as you can! You need to double the integer and return it.
-
-function doubleInteger(i) {
-  return i + i;
-}
-
-console.log(doubleInteger(2));

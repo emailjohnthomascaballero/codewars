79b46a5b37236dc77e571022faef036789555239
John <emailjohnthomascaballero@gmail.com>
2024-11-18 20:14:10 +0800

    Day 202

--- typescript/count-of-positives-sum-of-negatives.ts
+++ typescript/count-of-positives-sum-of-negatives.ts
@@ -11,7 +11,30 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 export function countPositivesSumNegatives(input: any) {
-  throw new Error('The method or operation is not implemented.');
+  // throw new Error('The method or operation is not implemented.');
+  if (input === null || input.length == 0) {
+    return [];
+  } else {
+    let positiveNumbers = input.filter((item: number) => item > 0).length;
+    let negativeNumbers = input
+      .filter((item: number) => item < 0)
+      .reduce(
+        (accumulator: number, currentValue: number) =>
+          accumulator + currentValue
+      );
+    // return positiveNumbers.concat(negativeNumbers)
+    // return positiveNumbers
+    return [positiveNumbers, negativeNumbers];
+  }
 }
 
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
+console.log(countPositivesSumNegatives(null));
+console.log(countPositivesSumNegatives([]));
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// reduce method have errors

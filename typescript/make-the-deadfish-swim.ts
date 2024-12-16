fce6d691c95935972b626fd3bf39392835121f6c
John <emailjohnthomascaballero@gmail.com>
2024-12-16 20:28:18 +0800

    Day 216

---
+++ typescript/make-the-deadfish-swim.ts
@@ -0,0 +1,24 @@
+// Make the Deadfish Swim
+
+// Create a parser to interpret and execute the Deadfish language.
+
+// Deadfish operates on a single value in memory, which is initially set to 0.
+
+// It uses four single-character commands:
+
+// i: Increment the value
+// d: Decrement the value
+// s: Square the value
+// o: Output the value to a result array
+// All other instructions are no-ops and have no effect.
+
+// Examples
+// Program "iiisdoso" should return numbers [8, 64].
+// Program "iiisdosodddddiso" should return numbers [8, 64, 3600].
+
+/** return the output array and ignore all non-op characters */
+export function parse(data: string): number[] {
+  return [1]
+}
+
+console.log(parse("iiisdoso"));

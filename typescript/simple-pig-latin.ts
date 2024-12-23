4a487260d3c8f961c866bfa6e3a2b283ee11b977
John <emailjohnthomascaballero@gmail.com>
2024-12-23 12:13:53 +0800

    Day 222

---
+++ typescript/simple-pig-latin.ts
@@ -0,0 +1,13 @@
+// Simple Pig Latin
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+export const pigIt = (a : string) : string =>  {
+  return "";
+}
+
+console.log(pigIt("Pig latin is cool"));

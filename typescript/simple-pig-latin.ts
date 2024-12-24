0a296b4730c43308e3998dab2f106734fe0f5e0f
John <emailjohnthomascaballero@gmail.com>
2024-12-24 19:49:52 +0800

    Day 223

--- typescript/simple-pig-latin.ts
+++ typescript/simple-pig-latin.ts
@@ -7,11 +7,12 @@
 // pigIt('Hello world !');     // elloHay orldway !
 
 export const pigIt = (a : string) : any =>  {
-  let words1 = a.split(' ').map(word => word.split('').slice(1).join('')).join(' ');
-  let words2 = a.split(' ').map(word => word.split('')[0]).join('');
-  // return `${words1} ${words2}`;
+  let secondWords = a.split(' ').map(word => word.split('').slice(1).join('')).join(' '); //second words
+  let firstWords = a.split(' ').map(word => word.split('')[0]).join(''); //first words
+  // return `${secondWords} ${words2}`;
   // ig atin s ool Plic
-  return words1.split('')
+  // return secondWords.split(' ').map(word => word + firstWords.split('').map(word => word).join('') + 'ay')
+  return secondWords
 }
 
 console.log(pigIt("Pig latin is cool"));

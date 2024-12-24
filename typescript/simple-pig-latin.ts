672a3a6c789220744c3136236327ec5a695ffaaa
John <emailjohnthomascaballero@gmail.com>
2024-12-24 19:31:36 +0800

    Day 223

--- typescript/simple-pig-latin.ts
+++ typescript/simple-pig-latin.ts
@@ -6,8 +6,12 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
-export const pigIt = (a : string) : string =>  {
-  return "";
+export const pigIt = (a : string) : any =>  {
+  let words1 = a.split(' ').map(word => word.split('').slice(1).join('')).join(' ');
+  let words2 = a.split(' ').map(word => word.split('')[0]).join('');
+  // return `${words1} ${words2}`;
+  // ig atin s ool Plic
+  
 }
 
 console.log(pigIt("Pig latin is cool"));

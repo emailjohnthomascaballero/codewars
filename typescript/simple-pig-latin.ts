1030606acd1399e5e7b76ff3f74ed3801b84fc10
John <emailjohnthomascaballero@gmail.com>
2024-12-26 11:24:29 +0800

    Day 225

--- typescript/simple-pig-latin.ts
+++ typescript/simple-pig-latin.ts
@@ -11,13 +11,19 @@ export const pigIt = (a : string) : any =>  {
   let secondWords = a.split(' ').map(word => word.split('').slice(1).join('')); //second words output: [ 'ig', 'atin', 's', 'ool' ]
   // let firstWords = a.split(' ').map(word => word.split('')[0]).join(''); //first words output: [ 'P', 'i', 'g' ]
   let firstWords = a.split(' ').map(word => word.split('')[0]); //first words
+  // let filterPunctuation =
   // return `${secondWords} ${words2}`;
   // ig atin s ool Plic
   // return secondWords.split(' ').map(word => word + firstWords.split('').map(word => word).join('') + 'ay')
   // return secondWords.split(' ').map(word => word + 'ay')
   // return secondWords.map((word, index) => word + firstWords[index] + 'ay' );
-  return secondWords.map((word, index) => `${word}${firstWords[index]}ay`).join(' ');
+  return secondWords.map((word, index) => `${word}${firstWords[index] == '!' || firstWords[index] == '?' || firstWords[index] == '.' ? firstWords[index] : firstWords[index] + 'ay'}`).join(' ');
+  // return secondWords.map((word, index) => word == '!' ? word : `${word}${firstWords[index]}ay`).join(' ');
+  // return secondWords.map((word, index) => word);
 }
 
 console.log(pigIt("Pig latin is cool"));
-console.log(pigIt("Hello world !")); // error! should not be "!ay"
+console.log(pigIt("Hello world !"));
+console.log(pigIt("This is my string ?"));
+console.log(pigIt("This is my string ."));
+//  error undefineday 

c265b932b6ea1319a6b124085a0f4b9d5387c617
John <emailjohnthomascaballero@gmail.com>
2024-12-30 19:12:10 +0800

    Day 229

--- typescript/simple-pig-latin.ts
+++ typescript/simple-pig-latin.ts
@@ -8,13 +8,18 @@
 
 export const pigIt = (a : string) : any =>  {
   // let secondWords = a.split(' ').map(word => word.split('').slice(1).join('')).join(' '); //second words
-  // let secondWords = a.split(' ').map(word => word.split('').slice(1).join('')); //second words output: [ 'ig', 'atin', 's', 'ool' ]
+  // let secondWords = a.split(' ').map(word => word === '' ? word : word.split('').slice(1).join('')); //second words output: [ 'ig', 'atin', 's', 'ool' ]
 
-  let secondWords = a.split(' ').map(word => word === '' ? null : word.split('').slice(1).join('')).filter(word => word !== null);
+  let secondWords = a.split(' ').map(word => word === '' || word === ' ' ? null : word.split('').slice(1).join('')).filter(word => word !== null);
+  // let secondWords = a.split(' ').map(word => word === '' || word === ' ' ? null : word.split('').slice(1).join('')).filter(word => word !== null);
+
   // let firstWords = a.split(' ').map(word => word.split('')[0]).join(''); //first words output: [ 'P', 'i', 'g' ]
   // let firstWords = a.split(' ').map(word => word.split('')[0]); //first words
+  // let firstWords = a.split(' ').map(word => word === '' ? word : word.split('')[0]); //first words
 
-  let firstWords = a.split(' ').map(word => word === '' ? null : word.split('')[0]).filter(word => word !== null)
+  let firstWords = a.split(' ').map(word => word === '' || word === ' ' ? null : word.split('')[0]).filter(word => word !== null)
+  // let firstWords = a.split(' ').map(word => word === '' || word === ' ' ? null : word.split('')[0]).filter(word => word !== null)
+
   // let filterPunctuation =
   // return `${secondWords} ${words2}`;
   // ig atin s ool Plic
@@ -23,6 +28,7 @@ export const pigIt = (a : string) : any =>  {
   // return secondWords.map((word, index) => word + firstWords[index] + 'ay' );
   // return secondWords.map((word, index) => `${word}${firstWords[index] == '!' || firstWords[index] == '?' || firstWords[index] == '.' ? firstWords[index] : firstWords[index] + 'ay'}`).join(' ');
 
+  // return secondWords.map((word, index) => `${word}${['!', '?', '.'].includes(firstWords[index]) ? firstWords[index] : firstWords[index] + 'ay'}`).join(' ');
   return secondWords.map((word, index) => `${word}${['!', '?', '.'].includes(firstWords[index]) ? firstWords[index] : firstWords[index] + 'ay'}`).join(' ');
 
   // return secondWords.map((word, index) => word == '!' ? word : `${word}${firstWords[index]}ay`).join(' ');
@@ -33,6 +39,6 @@ export const pigIt = (a : string) : any =>  {
 
 // console.log(pigIt("Pig latin is cool"));
 // console.log(pigIt("Hello world !"));
-console.log(pigIt("This  is my string ?")); //with whitespace
+console.log(pigIt("This   is my string ?")); //with whitespace
 // console.log(pigIt("This is my string ."));
-//  error undefineday // white space error // maybe trim() will help
+//  error undefineday // should ignore white space not removing them!
